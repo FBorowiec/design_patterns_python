95e4291e81d6132d387eef491b2b6e4e2650d679
Franek Borowiec <franek.borowiec@gmail.com>
2021-05-10 20:11:54 +0200

    Monostate second example

--- creational_patterns/singleton_pattern/monostate.py
+++ creational_patterns/singleton_pattern/monostate.py
@@ -16,6 +16,20 @@ class CEO:
     def __str__(self):
         return f"{self.name} is {self.age} year old"
 
+class Monostate:
+    __shared_state = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(Monostate, cls).__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls.__shared_state
+        return obj
+
+class CFO(Monostate):
+    def __init__(self):
+        self.name = ""
+
+    def __str__(self):
+        return f'{self.name}'
 
 import unittest
 
@@ -30,6 +44,15 @@ class MonostatePattern(unittest.TestCase):
         print(ceo1)
         print(ceo2)
 
+    def test_monostate_pattern2(self):
+        cfo1 = CFO()
+        cfo1 = "Babe"
+        print(cfo1)
+
+        cfo2 = CFO()
+        cfo2 = "Ruth"
+        print(cfo1)
+        print(cfo2)
 
 if __name__ == "__main__":
     unittest.main()
