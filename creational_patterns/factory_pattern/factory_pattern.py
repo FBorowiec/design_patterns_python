b90b751047d18a9f5b286add55c48950f8952686
Franek Borowiec <franek.borowiec@gmail.com>
2021-08-23 22:40:41 +0200

    Factory pattern

--- creational_patterns/factory_pattern/factory_pattern.py
+++ creational_patterns/factory_pattern/factory_pattern.py
@@ -36,16 +36,21 @@ class PointBad:
 
 
 class PointGood:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     def __str__(self):
         return f"x: {self.x}, y: {self.y}"
 
+
+class PointFactory:
     @staticmethod
     def new_cartesian_point(x, y):
-        return PointGood(x, y)
+        p = PointGood()
+        p.x = x
+        p.y = y
+        return p
 
     @staticmethod
     def new_polar_point(rho, theta):
@@ -58,7 +63,7 @@ import unittest
 class FactoryPattern(unittest.TestCase):
     def test_factory_pattern(self):
         p = PointGood(2, 3)
-        p2 = PointGood.new_polar_point(1, 2)
+        p2 = PointFactory.new_polar_point(1, 2)
         print(p, p2)
 
 
