41263829bf3aba056af2dced8117d309c45d6c28
Franek Borowiec <franek.borowiec@gmail.com>
2023-05-07 21:19:34 +0200

    Type hints

--- creational_patterns/builder_pattern/builder_pattern.py
+++ creational_patterns/builder_pattern/builder_pattern.py
@@ -7,11 +7,8 @@
 
 Builder - When piecewise object construction is complicated,
           provide and API for doing it succinctly
-"""
 
-"""
 # This is a bad way of creating html code in this case
-
 words = ["hello", "world"]
 parts = ["<ul>"]
 for w in words:
@@ -20,61 +17,59 @@ parts.append("</ul>")
 print('\n'.join(parts))
 
 """
+import unittest
 
 
 class HtmlElement:
-    indent_size = 2
+    INDENT_SIZE: int = 2
 
-    def __init__(self, name="", text=""):
-        self.text = text
-        self.name = name
-        self.elements = []
+    def __init__(self, name: str = "", text: str = ""):
+        self.text: str = text
+        self.name: str = name
+        self.elements: list[HtmlElement] = []
 
-    def __str(self, indent):
-        lines = []
-        i = " " * (indent * self.indent_size)
+    def to_string(self, indent: int) -> str:
+        lines: list[str] = []
+        i: str = " " * (indent * self.INDENT_SIZE)
         lines.append(f"{i}<{self.name}>")
 
         if self.text:
-            i1 = " " * ((indent + 1) * self.indent_size)
+            i1 = " " * ((indent + 1) * self.INDENT_SIZE)
             lines.append(f"{i1}{self.text}")
 
         for e in self.elements:
-            lines.append(e.__str(indent + 1))
+            lines.append(e.to_string(indent + 1))
 
         lines.append(f"{i}</{self.name}>")
         return "\n".join(lines)
 
-    def __str__(self):
-        return self.__str(0)
+    def __str__(self) -> str:
+        return self.to_string(0)
 
 
 class HtmlBuilder:
-    def __init__(self, root_name):
-        self.root_name = root_name
-        self.__root = HtmlElement(root_name)
+    def __init__(self, root_name: str):
+        self.root_name: str = root_name
+        self.__root: HtmlElement = HtmlElement(root_name)
 
-    def add_child(self, child_name, child_text):
+    def add_child(self, child_name: str, child_text: str) -> None:
         self.__root.elements.append(HtmlElement(child_name, child_text))
 
-    def add_child_fluent(self, child_name, child_text):
+    def add_child_fluent(self, child_name: str, child_text: str) -> "HtmlBuilder":
         self.__root.elements.append(HtmlElement(child_name, child_text))
         return self
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.__root)
 
     @staticmethod
-    def create(name):
+    def create(name: str) -> "HtmlBuilder":
         return HtmlBuilder(name)
 
 
-import unittest
-
-
 class BuilderPattern(unittest.TestCase):
-    def test_builder_pattern(self):
-        builder = HtmlBuilder("ul")
+    def test_builder_pattern(self) -> None:
+        builder: HtmlBuilder = HtmlBuilder("ul")
         # builder.add_child("li", "hello")
         # builder.add_child("li", "world")
         builder.add_child_fluent("li", "hello").add_child_fluent("li", "world")
